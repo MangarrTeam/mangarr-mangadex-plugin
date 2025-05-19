cc28e5cdd6e76c9abca653ffe3d3b4d632a3c55c
Makhuta <matykubla@gmail.com>
2025-05-20 00:37:50 +0200

    Initial push
    - have search

---
+++ core/mangadex/__init__.py
@@ -0,0 +1,142 @@
+from plugins.base import MangaPluginBase, Formats, AgeRating
+import sys
+import requests
+import json
+import logging
+logger = logging.getLogger(__name__)
+
+class MangaDex(MangaPluginBase):
+    languages = ["en","pt","pt-br","it","de","ru","aa","ab","ae","af","ak","am","an","ar-ae","ar-bh","ar-dz","ar-eg","ar-iq","ar-jo","ar-kw","ar-lb","ar-ly","ar-ma","ar-om","ar-qa","ar-sa","ar-sy","ar-tn","ar-ye","ar","as","av","ay","az","ba","be","bg","bh","bi","bm","bn","bo","br","bs","ca","ce","ch","co","cr","cs","cu","cv","cy","da","de-at","de-ch","de-de","de-li","de-lu","div","dv","dz","ee","el","en-au","en-bz","en-ca","en-cb","en-gb","en-ie","en-jm","en-nz","en-ph","en-tt","en-us","en-za","en-zw","eo","es-ar","es-bo","es-cl","es-co","es-cr","es-do","es-ec","es-es","es-gt","es-hn","es-la","es-mx","es-ni","es-pa","es-pe","es-pr","es-py","es-sv","es-us","es-uy","es-ve","es","et","eu","fa","ff","fi","fj","fo","fr-be","fr-ca","fr-ch","fr-fr","fr-lu","fr-mc","fr","fy","ga","gd","gl","gn","gu","gv","ha","he","hi","ho","hr-ba","hr-hr","hr","ht","hu","hy","hz","ia","id","ie","ig","ii","ik","in","io","is","it-ch","it-it","iu","iw","ja","ja-ro","ji","jv","jw","ka","kg","ki","kj","kk","kl","km","kn","ko","ko-ro","kr","ks","ku","kv","kw","ky","kz","la","lb","lg","li","ln","lo","ls","lt","lu","lv","mg","mh","mi","mk","ml","mn","mo","mr","ms-bn","ms-my","ms","mt","my","na","nb","nd","ne","ng","nl-be","nl-nl","nl","nn","no","nr","ns","nv","ny","oc","oj","om","or","os","pa","pi","pl","ps","pt-pt","qu-bo","qu-ec","qu-pe","qu","rm","rn","ro","rw","sa","sb","sc","sd","se-fi","se-no","se-se","se","sg","sh","si","sk","sl","sm","sn","so","sq","sr-ba","sr-sp","sr","ss","st","su","sv-fi","sv-se","sv","sw","sx","syr","ta","te","tg","th","ti","tk","tl","tn","to","tr","ts","tt","tw","ty","ug","uk","ur","us","uz","ve","vi","vo","wa","wo","xh","yi","yo","za","zh-cn","zh-hk","zh-mo","zh-ro","zh-sg","zh-tw","zh","zu"]
+    base_url = "https://mangadex.org/"
+    api_url = f"https://api.mangadex.org/"
+
+    def search_manga(self, query, language=None):
+        logger.debug(f'Searching for "{query}"')
+        limit = 100
+        offset = 0
+        total = sys.maxsize
+
+        mangaData = []
+        while offset < total:
+            try:
+                response = requests.get(f'{self.api_url}/manga',
+                                        params={
+                                            "title": query,
+                                            "limit": limit,
+                                            "offset": offset,
+                                            "contentRating[]": ["safe", "suggestive", "erotica", "pornographic"],
+                                            "includes[]": ["manga", "cover_art", "author", "artist", "tag"],
+                                        },
+                                        timeout=10
+                                        )
+                response.raise_for_status()
+
+                result = response.json()
+
+                offset += limit
+                if result is None:
+                    break
+
+                if "total" in result:
+                    total = int(result.get("total"))
+                else:
+                    continue
+
+                if "data" in result:
+                    result_datas = result.get("data")
+                    for result_data in result_datas:
+                        manga_dict = self.search_manga_dict()
+                        {
+                            "name": "",
+                            "description": "",
+                            "genres": [],
+                            "tags": [],
+                            "complete": False,
+                            "cover": None,
+                        }
+                        attributes = result_data.get("attributes", {})
+                        titles = attributes.get("title", {})
+                        if language is None:
+                            for key, value in titles.items():
+                                manga_dict["name"] = value
+                                break
+                        else:
+                            manga_dict["name"] = titles.get(language)
+                        if manga_dict["name"] is None:
+                            continue
+                        
+                        descriptions = attributes.get("description", {})
+                        manga_dict["description"] = descriptions.get(language)
+                        if manga_dict["description"] is None:
+                            for key, value in descriptions.items():
+                                manga_dict["description"] = value
+                                break
+                        
+                        tags = attributes.get("tags", [])
+                        for tag in tags:
+                            tag_attributes = tag.get("attributes", {})
+                            tag_group = tag_attributes.get("group")
+                            if tag_group is not None and tag_group == "genre":
+                                tag_names = tag_attributes.get("name")
+                                tag_name = tag_names.get(language)
+                                if tag_name is None:
+                                    for key, value in tag_names.items():
+                                        manga_dict["genres"].append(value)
+                                        break
+                                else:
+                                    manga_dict["genres"].append(tag_name)
+                            if tag_group is not None and tag_group == "theme":
+                                tag_names = tag_attributes.get("name")
+                                tag_name = tag_names.get(language)
+                                if tag_name is None:
+                                    for key, value in tag_names.items():
+                                        manga_dict["tags"].append(value)
+                                        break
+                                else:
+                                    manga_dict["tags"].append(tag_name)
+
+                        status = attributes.get("status")
+                        manga_dict["complete"] = False
+                        if status == "completed" or status == "cancelled":
+                            manga_dict["complete"] = True
+                        
+
+                        id = result_data.get("id")
+                        cover_url = "/uploads/static/no_thumbnail.png"
+                        if id is not None:
+                            manga_dict["id"] = id
+                            manga_dict["url"] = f"{self.base_url}title/{id}"
+                            relationships = result_data.get("relationships")
+                            if relationships is not None:
+                                for relationship in relationships:
+                                    if relationship.get("type") == "cover_art":
+                                        cover_fileName = relationship.get("attributes", {}).get("fileName")
+                                        if relationships is not None:
+                                            cover_url = f"https://uploads.mangadex.org/covers/{id}/{cover_fileName}"
+                                        break
+
+                            manga_dict["cover"] = cover_url
+                            mangaData.append(manga_dict)
+            except Exception as e:
+                logger.error(f'Error while searching manga - {e}')
+                break
+
+        return mangaData
+
+    def get_manga(self, url):
+        return self.get_manga_dict()
+    
+    def get_chapter(self, url):
+        return self.get_chapter_dict()
+    
+    def get_volume(self, url):
+        return self.get_volume_dict()
+    
+    def get_volumes(self, url):
+        return [self.get_volumes_dict()]
+    
+    def get_chapters(self, url):
+        return [self.get_chapters_dict()]
+    
+    def get_pages(self, url):
+        return [self.get_pages_dict()]
